9c740ca3b0fe7150612655942300edc05bd26f2d
kotekote07 <dakotablevins0310@gmail.com>
2023-07-08 16:48:07 -0400

    change base url

--- client/src/http-common.js
+++ client/src/http-common.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-    baseURL: "http://localhost:5000",
+    baseURL: "https://murmuring-citadel-90338-42ba0518069d.herokuapp.com",
     headers: {
         "Content-type": "application/json"
     }
